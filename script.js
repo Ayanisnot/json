dfdd485f679ef24554cd73fe1332d7fb665f5853
Ayanisnot <alayanhussain0@gmail.com>
2025-03-20 15:41:44 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var mystudent = {
-   name : "ronaldo",
-   age : 40,
-   grade : 9
-};
-var myJSON = JSON.stringify(mystudent);
-var newStudentObj = JSON.parse(myJSON);
-document.getElementById("result").innerHTML = 
-newStudentObj.name;
+function showtime() {
+var date = new Date();
+var h = date.getHours();
+var m = date.getMinutes();
+var s = date.getSeconds();
+var session = "AM";
+
+ if(h == 0) {
+    h = 12;
+ }
+
+
+ if(h > 12) {
+    h = h - 12;
+    session = "PM";
+ }
+
+ h = (h > 10) ? "0" + h : h;
+ m = (m > 10) ? "0" + m : m;
+ s = (s > 10) ? "0" + s : s;
+
+ var time = h + ":" + m + ":" + s + " " + session;
+ document.getElementById("myclockdisplay").innerText =time;
+ document.getElementById("myclockdisplay").textContent = time;
+
+ setTimeout(showtime , 1000);
+}
+showtime();
