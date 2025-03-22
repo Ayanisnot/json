3b70fdcc7ea833d1cdf10cd52ac137a6261c58ab
Ayanisnot <alayanhussain0@gmail.com>
2025-03-22 23:51:32 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-function showtime() {
-var date = new Date();
-var h = date.getHours();
-var m = date.getMinutes();
-var s = date.getSeconds();
-var session = "AM";
-
- if(h == 0) {
-    h = 12;
- }
-
-
- if(h > 12) {
-    h = h - 12;
-    session = "PM";
- }
-
- h = (h > 10) ? "0" + h : h;
- m = (m > 10) ? "0" + m : m;
- s = (s > 10) ? "0" + s : s;
-
- var time = h + ":" + m + ":" + s + " " + session;
- document.getElementById("myclockdisplay").innerText =time;
- document.getElementById("myclockdisplay").textContent = time;
-
- setTimeout(showtime , 1000);
+product = (a,b) =>  a*b;
+try {
+ document.getElementById("result").innerHTML = a;
 }
-showtime();
+catch(err) {
+ document.getElementById("result").innerHTML = err.message;
+}
